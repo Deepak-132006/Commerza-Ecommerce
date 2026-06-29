d226d170dc04ff705ac4c0da859651c31606120b
Deepak-132006 <deepakn132006@gmail.com>
2026-06-29 10:54:50 +0530

    Authcontext Created

---
+++ client/src/context/AuthProvider.jsx
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from 'react'
+
+export const AuthContext = createContext()
+export function AuthProvider({children}){
+    const [isLogged, setIsLogged] = useState(
+        !!localStorage.getItem("accessToken")
+    )
+  return (
+    <AuthContext.Provider value={{isLogged, setIsLogged}}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -18,8 +18,6 @@ const Login = () => {
     setShowPassword((prev) => !prev);
   };
   
-
-
   const validatePassword = (password) => {
     const regex =
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&^#()_+\-=\[\]{};':"\\|,.<>\/~`])[A-Za-z\d@$!%*?&^#()_+\-=\[\]{};':"\\|,.<>\/~`]{8,}$/;
