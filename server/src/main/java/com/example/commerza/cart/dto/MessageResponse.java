7919879defa353018c07beec0036f81d2f7a039f
Deepak-132006 <deepakn132006@gmail.com>
2026-07-02 21:35:12 +0530

    feat: Cart - Entity, DTOs Completed

---
+++ server/src/main/java/com/example/commerza/cart/dto/MessageResponse.java
@@ -0,0 +1,13 @@
+package com.example.commerza.cart.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessageResponse {
+
+    private String message;
+}
